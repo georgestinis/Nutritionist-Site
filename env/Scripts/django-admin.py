834ea8b242fb495cf4952956d12a626865e88fbf
george stinis <george.stinis@yahoo.gr>
2019-08-21 15:44:45 +0300

    Add files

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\stini\desktop\elpgeoproject\nutritionist_site\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
